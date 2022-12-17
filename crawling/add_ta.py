80f5502006c8c666072b79eb3fb3895f59dcc54e
choeunhak <02069@naver.com>
2022-12-17 20:05:52 +0900

    ta 함께 csv로 저장 기능 추가

---
+++ crawling/add_ta.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import pandas_ta as ta
+
+if __name__ == '__main__':
+
+    file_name = "TQQQ_1d_2012-01-14_2022-12-12"
+    # Load data
+    df = pd.read_csv(r"C:\Users\eunhak\Documents\project\stock_backtesting_platform\data\{}.csv".format(file_name), sep=",")
+    # inverse_df = pd.read_csv(r"C:\Users\eunhak\Documents\project\stock_backtesting_platform\data\SQQQ_1d_2019-01-14_2022-12-12.csv", sep=",")
+
+
+    # # Create your own Custom Strategy
+    # DayStochasticStrategy = ta.Strategy(
+    #     name="stochastic",
+    #     description="stochastic",
+    #     ta=[
+    #         {"kind": "stoch"},
+    #     ]
+    # )
+
+    # DayRsiStrategy = ta.Strategy(
+    #     name="rsi",
+    #     description="rsi",
+    #     ta=[
+    #         {"kind": "rsi"},
+    #     ]
+    # )
+
+    MyStrategy = ta.Strategy(
+        name="rsi",
+        description="rsi",
+        ta=[
+            {"kind": "rsi"},
+            {"kind": "stoch"},
+            {"kind": "macd"}
+        ]
+    )
+
+    # 전략설정
+    df.ta.strategy(MyStrategy)
+
+    # 저장
+    df.to_csv("./data/ta_{0}.csv".format(file_name), mode = "w")
