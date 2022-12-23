65b8e3d835fe12443513f7966cfc3302095de9bf
choeunhak <02069@naver.com>
2022-12-24 02:21:35 +0900

    주식들에 대해 반복문 적용하도록 수정

--- crawling/add_ta_to_df.py
+++ crawling/add_ta_to_df.py
@@ -1,25 +1,29 @@
 import pandas as pd
 import pandas_ta as ta
 
+stocks = ["QQQ", "QLD", "TQQQ", "PSQ", "SQQQ", "DIA", "ARKK", "IVV", "SSO", "UPRO", "FNGU", "BULZ"]
+
 if __name__ == '__main__':
 
-    file_name = "TQQQ_1d_2012-01-14_2022-12-12"
-    # Load data
-    df = pd.read_csv(r"C:\Users\eunhak\Documents\project\stock_backtesting_platform\data\{}.csv".format(file_name), sep=",")
-    # inverse_df = pd.read_csv(r"C:\Users\eunhak\Documents\project\stock_backtesting_platform\data\SQQQ_1d_2019-01-14_2022-12-12.csv", sep=",")
+    for stock in stocks:
+        file_name = "{}_1d_2012-01-03_2022-12-22".format(stock)
+        # Load data
+        df = pd.read_csv(r"C:\Users\eunhak\Documents\project\stock_backtesting_platform\data\{}.csv".format(file_name), sep=",")
+        # inverse_df = pd.read_csv(r"C:\Users\eunhak\Documents\project\stock_backtesting_platform\data\SQQQ_1d_2019-01-14_2022-12-12.csv", sep=",")
 
-    MyStrategy = ta.Strategy(
-        name="rsi",
-        description="rsi",
-        ta=[
-            {"kind": "rsi"},
-            {"kind": "stoch"},
-            {"kind": "macd"}
-        ]
-    )
+        MyStrategy = ta.Strategy(
+            name="rsi",
+            description="rsi",
+            ta=[
+                {"kind": "rsi"},
+                {"kind": "stoch"},
+                {"kind": "macd"},
+                {"kind": "mfi"},
+            ]
+        )
 
-    # 전략설정
-    df.ta.strategy(MyStrategy)
+        # 전략설정
+        df.ta.strategy(MyStrategy)
 
-    # 저장
-    df.to_csv("./data/ta_{0}.csv".format(file_name), mode = "w")
+        # 저장
+        df.to_csv("./ta_data/ta_{0}.csv".format(file_name), mode = "w")
